ae9a37444105a3475a35a1b70b51cf098355d305
kobra1789@gmail.com <nat.n.rozhkova@gmail.com>
2024-07-11 09:40:03 +0100

    create java app with maven

--- src/main/java/com/example/azurejavaapp/AzureJavaAppApplication.java
+++ src/main/java/com/example/azurejavaapp/AzureJavaAppApplication.java
@@ -1,7 +1,14 @@
 package com.example.azurejavaapp;
 
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class AzureJavaAppApplication {
 
+    @Autowired
+    private  SecretClient client;
+
+
     public static void main(String[] args) {
         SpringApplication.run(AzureJavaAppApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @GetMapping("/")
+    public String sayHello() {
+
+        return String.format("Secret name deep-secret value is  %s!",  client.getSecret("deep-secret").getValue());
     }
 
 }
